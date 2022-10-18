262ece7cb5d44da07c61b32eec4dc79f80e52607
Samuel-Kenrick <115630032+Samuel-Kenrick@users.noreply.github.com>
2022-10-18 10:52:53 +0200

    Add files via upload

--- run3.m
+++ run3.m
@@ -3,23 +3,26 @@ close all
 clear all
 N=2^6
 x = 0:0.001:2*pi;
-z= mydft(h(x))
+z= mydft(h(x));
 [a0,a,b] =myfouriercoeff(z);
 t = linspace(0,2*pi,5000);
 M= [3 5 10];% the number of terms M must be less than N/2 where N = 2m points
+%M = N/2;
 y=a0;
 for k = 1:length(M)
     y=a0;
     for j=1:M(k) % The partial sum of the Fourier series with M+1 terms
         y = y+a(j)*cos(j*t)+b(j)*sin(j*t);% add a term of the Fourier series
     end
-    p(k,:)=y(1,:)
+    p(k,:)=y(1,:);
 end
 
 figure()
-plot(t,h(t),t,p(1,:),t,p(2,:),t,p(3,:))
-axis([0 2*pi -1.5 1.5])
-xlabel('')
+plot(t,h(t),t,p(1,:),t,p(2,:),t,p(3,:));
+axis([0 2*pi -1.5 1.5]);
+legend('Normal t values', 'M=3', 'M=5', 'M=10')
+xlabel('t');
+ylabel('h(t)');
 hold off
 
 % plot h together with several of the partial sums of the Fourier series
--- run3b.m
+++ run3b.m
@@ -6,7 +6,7 @@ x = linspace(0,2*pi,5000);
 z= mydft(h(x))
 [a0,a,b] =myfouriercoeff(z);
 t = linspace(0,2*pi,5000);
-M= [3 5 10];% the number of terms M must be less than N/2 where N = 2m points
+M= 10^6/2;% the number of terms M must be less than N/2 where N = 2m points
 y=a0;
 clear j k
 
@@ -21,9 +21,18 @@ for k = 1:100
     E_M(k,1) = max(r(k,1:round(k/2))-1);
 end
 clear j k
+for k = 1:100
+    yExact=y0
+    for J=1:k
+        yExact=yExact+2/(pi*j)*(1-(-1)^n);
+    end
+    p(k,:)=yExact(1,:)
+end
+
+clear j k
 k = linspace(1,100);
 subplot(2, 2, [1,2]);
-plot(t,h(t),t,r);
+plot(t,h(t),t,r, t, p);
 
 axis([0 2*pi -1.5 1.5]);
 xlabel('t');
